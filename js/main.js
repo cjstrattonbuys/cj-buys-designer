358ed6aa4b63638039ae67402fb0a94126be2094
CJ Buys <cjstrattonbuys@gmail.com>
2022-09-13 21:49:10 -0400

    Added image gallery to Sit Conmigo case study

--- js/main.js
+++ js/main.js
@@ -15,3 +15,29 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+
+var images = document.getElementsByTagName("img");
+
+for (var i = 0; i < images.length; i++) {
+  images[i].onmouseover = function() {
+    this.style.cursor = "hand";
+    this.style.borderColor = "var(--color4)";
+  };
+  images[i].onmouseout = function() {
+    this.style.cursor = "pointer";
+    this.style.borderColor = "var(--color2)";
+  };
+}
+
+function changeImageOnClick(event) {
+  // debugger;
+  var targetElement = event.srcElement;
+  // debugger;
+  if (targetElement.tagName === "IMG") {
+    mainImage.src = targetElement.getAttribute("src");
+  }
+}
+
+
+
